a2058cebba086e35550b603a68f1e0d13b5f530c
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-10-21 10:22:47 +0200

    COś tam

---
+++ c/lab3/ex1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int getNumber() {
+  int n;
+  printf("Podaj liczbę: ");
+  fflush( stdout );
+  scanf("%d", &n);
+  return n;
+}
+
+int findBinary(int n) {
+  printf("Postać binarna to: ");
+
+  for (int i = 31; i >= 0; i--) {
+    if ((i + 1) % 8 == 0) printf(" ");
+    printf("%1d", (( n >> i ) % 2));
+  }
+
+  return 0;
+}
+
+int main() {
+  int number;  
+  
+  number = getNumber();
+  findBinary(number);
+  printf("\n");
+
+  return 0;
+}
